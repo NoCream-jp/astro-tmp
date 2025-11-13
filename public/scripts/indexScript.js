5bcaccce862ce2ccc268662a7278edfba68ce411
NoCream <occ3710@outlook.jp>
2025-11-13 13:25:19 +0900

    footerのデザイン、bodyのflex-box化、その他微調整

--- public/scripts/indexScript.js
+++ public/scripts/indexScript.js
@@ -33,25 +33,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// スクロール時にヘッダーのスタイルを変更する
-document.addEventListener('DOMContentLoaded', () => {
-    const header = document.querySelector('.header');
-  
-    // ヘッダー要素が存在する場合のみ処理を実行
-    if (header) {
-      window.addEventListener('scroll', () => {
-        // 50px以上スクロールされたら 'scrolled' クラスを追加
-        if (window.scrollY > 50) {
-          header.classList.add('scrolled');
-        } else {
-          // スクロールが50px未満の場合は 'scrolled' クラスを削除
-          header.classList.remove('scrolled');
-        }
-      });
-    }
-  });
-  
-
 document.addEventListener("DOMContentLoaded", () => {
     const fadeInElements = document.querySelectorAll(".fade-in");
 
